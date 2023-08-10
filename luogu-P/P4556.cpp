dad97a0710eb979e5e611effd72541068668912a
ncwzdlsd <lyn_666888@outlook.com>
2023-08-10 10:07:31 +0800

    9

--- luogu-P/P4556.cpp
+++ luogu-P/P4556.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn=2e5+5,mxr=1e5,maxm=1e7+5;
+const int maxn=2e5+5,sumr=1e5,maxm=1e7+5;
 int head[maxn],nxt[maxn],to[maxn],f[maxn][25],cnt,dep[maxn],rot[maxn],ans[maxn];
 
-struct node{int ls,rs,sum,mx;}t[maxm];
+struct node{int ls,rs,id,sum;}t[maxm];
 
 void add(int x,int y){to[++cnt]=y;nxt[cnt]=head[x];head[x]=cnt;}
 
@@ -12,7 +12,7 @@ void dfs(int x,int fa)
 {
 	f[x][0]=fa;
 	dep[x]=dep[fa]+1;
-	for(int i=1;i<20;++i)
+	for(int i=1;i<=20;++i)
         f[x][i]=f[f[x][i-1]][i-1];
 	for(int i=head[x];i;i=nxt[i])
 	{
@@ -24,24 +24,24 @@ void dfs(int x,int fa)
 int lca(int x,int y)
 {
 	if(dep[x]<dep[y]) swap(x,y);
-	for(int i=25;i>=0;i--) if(dep[f[x][i]]>=dep[y]) x=f[x][i];
+	for(int i=20;i>=0;i--) if(dep[f[x][i]]>=dep[y]) x=f[x][i];
 	if(x==y) return x;
-	for(int i=25;i>=0;i--) if(f[x][i]!=f[y][i]) x=f[x][i],y=f[y][i];
+	for(int i=20;i>=0;i--) if(f[x][i]!=f[y][i]) x=f[x][i],y=f[y][i];
+	// printf("%d %d\n",x,y);
 	return f[x][0];
 }
 
 void pushup(int k)
 {
-	if(t[t[k].ls].mx>=t[t[k].rs].mx)
-        t[k].mx=t[t[k].ls].mx,t[k].sum=t[t[k].ls].sum;
-    else if(t[t[k].ls].mx<t[t[k].rs].mx)
-        t[k].mx=t[t[k].rs].mx,t[k].sum=t[t[k].rs].sum;
+	if((t[t[k].ls].sum>t[t[k].rs].sum)||(t[t[k].ls].sum==t[t[k].rs].sum&&t[t[k].ls].id<t[t[k].rs].id))
+        t[k].sum=t[t[k].ls].sum,t[k].id=t[t[k].ls].id;
+    else t[k].sum=t[t[k].rs].sum,t[k].id=t[t[k].rs].id;
 }
 
 void update(int &rt,int l,int r,int x,int v)
 {
 	if(!rt) rt=++cnt;
-	if(l==r) {t[rt].mx+=v,t[rt].sum=x;return;}
+	if(l==r) {t[rt].sum+=v,t[rt].id=x;return;}
 	int mid=(l+r)/2;
 	if(x<=mid) update(t[rt].ls,l,mid,x,v);
 	else update(t[rt].rs,mid+1,r,x,v);
@@ -51,7 +51,7 @@ void update(int &rt,int l,int r,int x,int v)
 void merge(int &p,int q,int l,int r)
 {
 	if(!p||!q) {p=p+q;return;}
-	if(l==r) {t[p].mx+=t[q].mx;return;}
+	if(l==r) {t[p].sum+=t[q].sum;return;}
 	int mid=(l+r)/2;
 	merge(t[p].ls,t[q].ls,l,mid),merge(t[p].rs,t[q].rs,mid+1,r);
 	pushup(p);
@@ -63,9 +63,10 @@ void calc(int x,int fa)
 	{
 		if(to[i]==fa) continue;
 		calc(to[i],x);
-		merge(rot[x],rot[to[i]],1,mxr);
+		merge(rot[x],rot[to[i]],1,sumr);
 	}
-	if(t[rot[x]].mx>0) ans[x]=t[rot[x]].sum;
+	// if(x==1)printf("%d %d\n",t[rot[x]].id,t[rot[x]].sum);
+	if(t[rot[x]].sum>0) ans[x]=t[rot[x]].id;
 }
 
 int main()
@@ -82,10 +83,11 @@ int main()
 	{
 		int x,y,z;cin>>x>>y>>z;
 		int tmp=lca(x,y);
-		update(rot[x],1,mxr,z,1);
-        update(rot[y],1,mxr,z,1);
-        update(rot[tmp],1,mxr,z,-1);
-        update(rot[f[tmp][0]],1,mxr,z,-1);	
+		// printf("%d %d %d--\n",x,y,tmp);
+		update(rot[x],1,sumr,z,1);
+        update(rot[y],1,sumr,z,1);
+        update(rot[tmp],1,sumr,z,-1);
+        update(rot[f[tmp][0]],1,sumr,z,-1);	
     }
     calc(1,0);
 	for(int i=1;i<=n;i++) cout<<ans[i]<<endl;
